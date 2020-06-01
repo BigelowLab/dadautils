7d0bf5f578d2bf4a845dfeb5e8715d3ee2e434a6
Ben Tupper <btupper@bigelow.org>
2020-06-01 10:54:16 -0400

    update with new storage path

--- R/path.R
+++ R/path.R
@@ -3,7 +3,7 @@
 #' @export
 #' @param ... arguments for \code{\link[base]{file.path}}
 #' @param root character, the root path
-edna_path <- function(..., root = "/mnt/storage/projects/edna"){
+edna_path <- function(..., root = "/mnt/storage/data/edna"){
   file.path(root, ...)
 } 
 
